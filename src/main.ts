131e3492dcfd45b75133ee6f006b40e0eda8f8b2
Zyear <cgu123456@qq.com>
2026-07-27 10:38:11 +0800

    revert(repo): 回滚目录迁移

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import { Icon } from '@iconify/vue'
 
 import App from './App.vue'
-import router from './router'
+import router from './router/index.ts'
 import './styles/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'element-plus/dist/index.css'
